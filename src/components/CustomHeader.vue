0976d32c37e20238d525e8fe61c063eccad9bf56
Leong Guangxian Daniel <danielleong.2023@scis.smu.edu.sg>
2024-10-08 21:55:08 +0800

    custom section header

---
+++ src/components/CustomHeader.vue
@@ -0,0 +1,38 @@
+<!-- components/CustomHeader.vue -->
+<template>
+	<div class="parent">
+		<div class="header"></div>
+		<span class="header-text">{{ header }}</span>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		header: String,
+	},
+};
+</script>
+
+<style scoped>
+.header {
+	background-color: #ff9689;
+	width: 90px;
+	padding: 1px 2px;
+}
+
+.header-text {
+	color: #efba1d;
+	text-align: left;
+	display: block;
+	font-family: "Cherry Bomb", sans-serif;
+	text-transform: uppercase;
+	font-weight: bold;
+	font-size: 13px;
+	text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
+		1px 1px 0 #000;
+}
+.parent {
+	padding: 10px 0px;
+}
+</style>
