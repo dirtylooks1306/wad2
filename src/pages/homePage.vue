58f9040cd1bb47346b39a322ec76f2a4146473d7
Leong Guangxian Daniel <danielleong.2023@scis.smu.edu.sg>
2024-10-14 16:51:25 +0800

    connected table to db

--- src/pages/homePage.vue
+++ src/pages/homePage.vue
@@ -6,67 +6,69 @@ import Forum from "../components/Forum.vue";
 </script>
 <template>
 	<NavBar />
-	<header>
-		<div class="jumbotron header_class">
-			<div class="row">
-				<div class="col-md-6">
-					<div class="title-class">
-						<span class="line-1">Empowering Parents</span><br />
-						<span class="line-2">Nurturing Futures</span>
+	<body>
+		<header>
+			<div class="jumbotron header_class">
+				<div class="row">
+					<div class="col-md-6 header">
+						<div class="title-class">
+							<span class="line-1">Empowering Parents</span><br />
+							<span class="line-2">Nurturing Futures</span>
+						</div>
+						<br />
+						<router-link to="/diary">
+							<button class="btn btn-primary">Get Started</button>
+						</router-link>
 					</div>
-					<br />
-					<router-link to="/diary">
-						<button class="btn btn-primary">Get Started</button>
+					<div class="col-md-6">
+						<img src="../assets/HomeMasthead.svg" alt="baby" />
+					</div>
+				</div>
+			</div>
+		</header>
+		<div class="secondary-background p-3">
+			<div class="container-fluid">
+				<CustomHeader header="Article" />
+				<h2>TRENDING ARTICLES</h2>
+				<Carousel :items="carouselItems" />
+			</div>
+		</div>
+
+		<div class="container-fluid p-3">
+			<CustomHeader header="GrowthTracker" />
+			<div class="row">
+				<div class="col-md-1"></div>
+				<div class="col-md-4 col-12 text-start">
+					<div class="title-class">
+						Begin Your Little One's Journey to Healthy Growth!
+					</div>
+					<p>
+						Stay informed and engaged—check out the Growth Tracker
+						to celebrate every milestone!
+					</p>
+					<router-link to="/tracker">
+						<button class="btn btn-primary">GrowthTracker</button>
 					</router-link>
 				</div>
-				<div class="col-md-6">
-					<img src="../assets/HomeMasthead.svg" alt="baby" />
+				<div class="col-md-6 col-12">
+					<img
+						src="../assets/homepageBaby.jpg"
+						alt="Growth Tracker"
+						class="carousel-image"
+					/>
 				</div>
+				<div class="col-md-1"></div>
 			</div>
 		</div>
-	</header>
-	<div class="secondary-background p-3">
-		<div class="container-fluid">
-			<CustomHeader header="Article" />
-			<h2>TRENDING ARTICLES</h2>
-			<Carousel :items="carouselItems" />
-		</div>
-	</div>
 
-	<div class="container-fluid p-3">
-		<CustomHeader header="GrowthTracker" />
-		<div class="row">
-			<div class="col-md-1"></div>
-			<div class="col-md-4 col-12 text-start">
-				<div class="title-class">
-					Begin Your Little One's Journey to Healthy Growth!
-				</div>
-				<p >
-					Stay informed and engaged—check out the Growth Tracker to
-					celebrate every milestone!
-				</p>
-				<router-link to="/tracker">
-					<button class="btn btn-primary">GrowthTracker</button>
-				</router-link>
-			</div>
-			<div class="col-md-6 col-12">
-				<img
-					src="../assets/homepageBaby.jpg"
-					alt="Growth Tracker"
-					class="carousel-image"
-				/>
-			</div>
-			<div class="col-md-1"></div>
+		<div class="container-fluid secondary-background p-3">
+			<CustomHeader header="Forum" />
+			<Forum v-for="post in forumPosts" :key="post.id" :post="post" />
 		</div>
-	</div>
-
-	<div class="container-fluid secondary-background p-3">
-		<CustomHeader header="Forum" />
-		<Forum v-for="post in forumPosts" :key="post.id" :post="post" />
-	</div>
-	<footer>
-		<p>CradleCare © 2024</p>
-	</footer>
+		<footer>
+			<p class="text-center">CradleCare © 2024</p>
+		</footer>
+	</body>
 </template>
 
 <script>
@@ -76,7 +78,7 @@ export default {
 		CustomHeader,
 		Carousel,
 		NavBar,
-		Forum
+		Forum,
 	},
 	data() {
 		return {
@@ -109,21 +111,21 @@ export default {
 					icon2: "src/assets/bookmark.svg",
 				},
 			],
-			forumPosts :[
+			forumPosts: [
 				{
 					id: 1,
-					icon: 'fa-car',
-					title: 'Description Title',
-					link: '#',
-					description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+					icon: "fa-car",
+					title: "Description Title",
+					link: "#",
+					description:
+						"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 					postsCount: 24,
 					topicsCount: 12,
-					lastPostLink: '#',
-					lastPoster: 'JustAUser',
-					lastPosterLink: '#',
-					lastPostDate: '12 Dec 2020'
+					lastPostLink: "#",
+					lastPoster: "JustAUser",
+					lastPosterLink: "#",
+					lastPostDate: "12 Dec 2020",
 				},
-				
 			],
 		};
 	},
@@ -161,6 +163,12 @@ h1 .line-2 {
 	color: #ff9689;
 	font-family: "Cherry Bomb", sans-serif;
 }
+.header {
+	margin: auto;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
 img {
 	max-width: 100%;
 	height: auto;
@@ -186,5 +194,8 @@ img {
 
 footer {
 	margin-top: 40px;
+	padding: 20px 0px;
+
 }
+
 </style>
--- src/pages/trackerPage.vue
+++ src/pages/trackerPage.vue
@@ -1,217 +1,277 @@
 <script setup>
 import NavBar from "../components/navBar.vue";
+import { db, collection, getDocs, doc } from "../firebaseConfig.js";
 </script>
 
 <template>
 	<NavBar />
 	<div class="container-fluid">
-		<div class="mt-3">
-			<ul class="d-md-flex desktop-tabs">
-				<li
-					:class="{ selected: activeTab === 'GrowthTracker' }"
-					@click="activeTab = 'GrowthTracker'"
-				>
-					<a href="#GrowthTracker">Growth Tracker</a>
-				</li>
-				<li
-					:class="{ selected: activeTab === 'Vaccine' }"
-					@click="activeTab = 'Vaccine'"
-				>
-					<a href="#Vaccine">Vaccination Tracker</a>
-				</li>
-			</ul>
+		<ul class="d-md-flex desktop-tabs mt-3">
+			<li
+				:class="{ selected: activeTab === 'GrowthTracker' }"
+				@click="activeTab = 'GrowthTracker'"
+			>
+				<a href="#GrowthTracker">Growth Tracker</a>
+			</li>
+			<li
+				:class="{ selected: activeTab === 'Vaccine' }"
+				@click="activeTab = 'Vaccine'"
+			>
+				<a href="#Vaccine">Vaccination Tracker</a>
+			</li>
+		</ul>
 
-			<!-- <select v-model="activeTab" class="mobile-dropdown" @change="changeTab">
+		<!-- <select v-model="activeTab" class="mobile-dropdown" @change="changeTab">
       <option value="GrowthTracker">Growth Tracker</option>
       <option value="Vaccine">Vaccination Tracker</option>
     </select> -->
-			<div v-if="activeTab === 'GrowthTracker'">bob</div>
-			<div v-else-if="activeTab === 'Vaccine'">bob2</div>
-			<table class="logging-table">
-				<thead>
-					<tr>
-						<th>Date</th>
-						<th>9-Sep</th>
-						<th>10-Sep</th>
-					</tr>
-				</thead>
+		<div v-if="activeTab === 'GrowthTracker'" class="container-fluid">
+			<table class="logging-table ">
 				<tbody>
 					<tr>
-						<td>Weight</td>
-						<td>1 kg</td>
-						<td>1.2 kg</td>
+						<th>Date of Record</th>
+						<td v-for="(post, index) in posts" :key="index">
+							{{ post.date || "" }}
+						</td>
+						<td
+							v-for="n in 10 - posts.length"
+							:key="n + posts.length"
+						></td>
 					</tr>
 					<tr>
-						<td>Height</td>
-						<td>4 cm</td>
-						<td>5 cm</td>
+						<th>Current age</th>
+						<td v-for="post in posts" :key="post.id">
+							{{ post.age || "" }}
+						</td>
+						<td
+							v-for="n in 10 - posts.length"
+							:key="n + posts.length"
+						></td>
 					</tr>
 					<tr>
-						<td>Head circumference</td>
-						<td>5 cm</td>
-						<td>5.5 cm</td>
+						<th>Weight (in kg)</th>
+						<td v-for="post in posts" :key="post.id">
+							{{ post.weight || "" }}
+						</td>
+						<td
+							v-for="n in 10 - posts.length"
+							:key="n + posts.length"
+						></td>
 					</tr>
 					<tr>
-						<td>Remarks</td>
-						<td colspan="2">Notes here</td>
+						<th>Height / Length (in cm)</th>
+						<td v-for="post in posts" :key="post.id">
+							{{ post.height || "" }}
+						</td>
+						<td
+							v-for="n in 10 - posts.length"
+							:key="n + posts.length"
+						></td>
+					</tr>
+					<tr>
+						<th>First steps</th>
+						<td v-for="n in 10" :key="n">
+							{{ posts[n - 1] ? posts[n - 1].steps : "" }}
+						</td>
+					</tr>
+					<tr>
+						<th>First words</th>
+						<td v-for="n in 10" :key="n">
+							{{ posts[n - 1] ? posts[n - 1].words : "" }}
+						</td>
+					</tr>
+					<tr>
+						<th>Remarks</th>
+						<td v-for="n in 10" :key="n">
+							{{ posts[n - 1] ? posts[n - 1].remarks : "" }}
+						</td>
 					</tr>
 				</tbody>
 			</table>
-		</div>
-		<div class="row pt-3">
-			<div class="input-container col-md-6 ps-5">
-				<div class="form-group row p-1">
-					<div class="col-md-1"></div>
-					<label for="date" class="col-md-3 col-12 col-form-label"
-						>Date of record</label
-					>
-					<div class="col-md-8 col-12">
-						<input type="date" class="form-control" id="date" v-model="selectedDate"/>
-					</div>
-				</div>
-				<div class="form-group row p-1">
-					<div class="col-md-1"></div>
-					<label for="age" class="col-md-3 col-12 col-form-label"
-						>Current age</label
-					>
-					<div class="col-md-8 col-12">
-						<select for="age" class="form-control" v-model="selectedAge">
-							<option value="0-2 months">0-2 months</option>
-							<option value="2-4 months">2-4 months</option>
-							<option value="4-6 months">4-6 months</option>
-							<option value="6-9 months">6-9 months</option>
-							<option value="9-12 months">9-12 months</option>
-							<option value="12-18 months">12-18 months</option>
-							<option value="18-24 months">18-24 months</option>
-						</select>
-					</div>
-				</div>
-				<div class="form-group row p-1">
-					<div class="col-md-1"></div>
-					<label for="date" class="col-md-3 col-12 col-form-label"
-						>Weight</label
-					>
-					<div class="col-md-8 col-12">
-						<input
-							type="number"
-							class="form-control"
-							id="weight"
-							placeholder="11.4 kg"
-              v-model="selectedWeight"
-						/>
-					</div>
-				</div>
-				<div class="form-group row p-1">
-					<div class="col-md-1"></div>
-					<label for="date" class="col-md-3 col-12 col-form-label"
-						>Height/ Length</label
-					>
-					<div class="col-md-8 col-12">
-						<input
-							type="number"
-							class="form-control"
-							id="date"
-							placeholder="30 cm"
-              v-model="selectedHeight"
-						/>
-					</div>
-				</div>
-				<div class="form-group row ps-1">
-					<div class="col-md-1"></div>
-					<label for="steps" class="col-md-3 col-12 col-form-label"
-						>First Steps?</label
-					>
-					<div class="col-md-8 col-12 pt-2">
-						<div class="form-check form-check-inline">
-							<input
-								class="form-check-input"
-								type="radio"
-								name="steps"
-								id="yesSteps"
-								value="yes"
-                v-model="selectedSteps"
-							/>
-							<label class="form-check-label" for="inlineRadio1"
-								>Yes</label
-							>
-						</div>
-						<div class="form-check form-check-inline">
-							<input
-								class="form-check-input"
-								type="radio"
-								name="steps"
-								id="noSteps"
-								value="no"
-							/>
-							<label class="form-check-label" for="inlineRadio2"
-								>No</label
-							>
-						</div>
-					</div>
-				</div>
-				<div class="form-group row ps-1">
-					<div class="col-md-1"></div>
-					<label for="words" class="col-md-3 col-12 col-form-label"
-						>First Words?</label
-					>
-					<div class="col-md-8 col-12 pt-2">
-						<div class="form-check form-check-inline">
-							<input
-								class="form-check-input"
-								type="radio"
-								name="words"
-								id="yesWords"
-								value="yes"
-							/>
-							<label class="form-check-label" for="inlineRadio1"
-								>Yes</label
-							>
-						</div>
-						<div class="form-check form-check-inline">
-							<input
-								class="form-check-input"
-								type="radio"
-								name="words"
-								id="noWords"
-								value="no"
-                v-model="selectedWords"
-							/>
-							<label class="form-check-label" for="inlineRadio2"
-								>No</label
-							>
-						</div>
-					</div>
-				</div>
-				<div class="form-group row p-1">
-					<div class="col-md-1"></div>
-					<label for="remarks" class="col-md-3 col-12 col-form-label"
-						>Remarks</label
-					>
-					<div class="col-md-8 col-12">
-						<textarea
-							class="form-control"
-							id="remarks"
-							rows="2"
-							placeholder="Notes here"
-              v-model="selectedRemarks"
-						></textarea>
-					</div>
-				</div>
-				<div class="row pt-3">
-					<div class="col-md-4"></div>
-					<div class="col-md-1">
-						<button type="submit" class="btn btn-primary">
-							Submit
-						</button>
-					</div>
-					<div class="col-md-7"></div>
-				</div>
-			</div>
 
-			<div class="col-md-6">
-				<img src="../assets/homepageBaby.jpg" class="w-50" />
+			<div class="row pt-3">
+				<div class="input-container col-md-6 ps-5">
+					<div class="form-group row p-1">
+						<div class="col-md-1"></div>
+						<label for="date" class="col-md-3 col-12 col-form-label"
+							>Date of record</label
+						>
+						<div class="col-md-8 col-12">
+							<input
+								type="date"
+								class="form-control"
+								id="date"
+								v-model="selectedDate"
+							/>
+						</div>
+					</div>
+					<div class="form-group row p-1">
+						<div class="col-md-1"></div>
+						<label for="age" class="col-md-3 col-12 col-form-label"
+							>Current age</label
+						>
+						<div class="col-md-8 col-12">
+							<select
+								for="age"
+								class="form-control"
+								v-model="selectedAge"
+							>
+								<option value="0-2 months">0-2 months</option>
+								<option value="2-4 months">2-4 months</option>
+								<option value="4-6 months">4-6 months</option>
+								<option value="6-9 months">6-9 months</option>
+								<option value="9-12 months">9-12 months</option>
+								<option value="12-18 months">
+									12-18 months
+								</option>
+								<option value="18-24 months">
+									18-24 months
+								</option>
+							</select>
+						</div>
+					</div>
+					<div class="form-group row p-1">
+						<div class="col-md-1"></div>
+						<label for="date" class="col-md-3 col-12 col-form-label"
+							>Weight</label
+						>
+						<div class="col-md-8 col-12">
+							<input
+								type="number"
+								class="form-control"
+								id="weight"
+								placeholder="11.4 kg"
+								v-model="selectedWeight"
+							/>
+						</div>
+					</div>
+					<div class="form-group row p-1">
+						<div class="col-md-1"></div>
+						<label for="date" class="col-md-3 col-12 col-form-label"
+							>Height/ Length</label
+						>
+						<div class="col-md-8 col-12">
+							<input
+								type="number"
+								class="form-control"
+								id="date"
+								placeholder="30 cm"
+								v-model="selectedHeight"
+							/>
+						</div>
+					</div>
+					<div class="form-group row ps-1">
+						<div class="col-md-1"></div>
+						<label
+							for="steps"
+							class="col-md-3 col-12 col-form-label"
+							>First Steps?</label
+						>
+						<div class="col-md-8 col-12 pt-2">
+							<div class="form-check form-check-inline">
+								<input
+									class="form-check-input"
+									type="radio"
+									name="steps"
+									id="yesSteps"
+									value="yes"
+									v-model="selectedSteps"
+								/>
+								<label
+									class="form-check-label"
+									for="inlineRadio1"
+									>Yes</label
+								>
+							</div>
+							<div class="form-check form-check-inline">
+								<input
+									class="form-check-input"
+									type="radio"
+									name="steps"
+									id="noSteps"
+									value="no"
+								/>
+								<label
+									class="form-check-label"
+									for="inlineRadio2"
+									>No</label
+								>
+							</div>
+						</div>
+					</div>
+					<div class="form-group row ps-1">
+						<div class="col-md-1"></div>
+						<label
+							for="words"
+							class="col-md-3 col-12 col-form-label"
+							>First Words?</label
+						>
+						<div class="col-md-8 col-12 pt-2">
+							<div class="form-check form-check-inline">
+								<input
+									class="form-check-input"
+									type="radio"
+									name="words"
+									id="yesWords"
+									value="yes"
+								/>
+								<label
+									class="form-check-label"
+									for="inlineRadio1"
+									>Yes</label
+								>
+							</div>
+							<div class="form-check form-check-inline">
+								<input
+									class="form-check-input"
+									type="radio"
+									name="words"
+									id="noWords"
+									value="no"
+									v-model="selectedWords"
+								/>
+								<label
+									class="form-check-label"
+									for="inlineRadio2"
+									>No</label
+								>
+							</div>
+						</div>
+					</div>
+					<div class="form-group row p-1">
+						<div class="col-md-1"></div>
+						<label
+							for="remarks"
+							class="col-md-3 col-12 col-form-label"
+							>Remarks</label
+						>
+						<div class="col-md-8 col-12">
+							<textarea
+								class="form-control"
+								id="remarks"
+								rows="2"
+								placeholder="Notes here"
+								v-model="selectedRemarks"
+							></textarea>
+						</div>
+					</div>
+					<div class="row pt-3">
+						<div class="col-md-4"></div>
+						<div class="col-md-1">
+							<button type="submit" class="btn btn-primary">
+								Submit
+							</button>
+						</div>
+						<div class="col-md-7"></div>
+					</div>
+				</div>
+
+				<div class="col-md-6">
+					<img src="../assets/homepageBaby.jpg" class="w-50" />
+				</div>
 			</div>
 		</div>
+		<div v-else-if="activeTab === 'Vaccine'">bob2</div>
 	</div>
 </template>
 
@@ -221,33 +281,63 @@ export default {
 	data() {
 		return {
 			activeTab: "GrowthTracker",
-      selectedDate: '',
-      selectedAge:"0-2 months",
-      selectedWeight: "",
-      selectedHeight: "",
-      selectedSteps: "",
-      selectedWords: "",
-      selectedRemarks: ""
+			selectedDate: "",
+			selectedAge: "0-2 months",
+			selectedWeight: "",
+			selectedHeight: "",
+			selectedSteps: "",
+			selectedWords: "",
+			selectedRemarks: "",
+			posts: [],
 		};
 	},
 	methods: {
 		changeTab() {},
-    currentDate(){
-      const today = new Date();
-      const day = String(today.getDate()).padStart(2, '0');
-      const month = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
-      const year = today.getFullYear();
+		currentDate() {
+			const today = new Date();
+			const day = String(today.getDate()).padStart(2, "0");
+			const month = String(today.getMonth() + 1).padStart(2, "0"); // January is 0!
+			const year = today.getFullYear();
 
-      let tdyDate = `${year}-${month}-${day}`;
-      return tdyDate;
-    }
+			let tdyDate = `${year}-${month}-${day}`;
+			return tdyDate;
+		},
 	},
-  created() {
-    this.selectedDate = this.currentDate();
-  },
-  mounted() {
-    window.vm = this; // Assign the Vue instance to a global variable
-  }
+	created() {
+		this.selectedDate = this.currentDate();
+	},
+	async mounted() {
+		window.vm = this;
+		// const postsCollectionRef = collection(db, "posts");
+		// const userDocs = await getDocs(postsCollectionRef);
+		// let arrayOfUsers = userDocs.docs;
+		// const allPosts = {};
+		// for (let indivUser of arrayOfUsers) {
+		// 	let userId = indivUser.id;
+		// 	let userPostsRef = collection(db, `posts/${userId}/post`);
+		// 	const postsSnapshot = await getDocs(userPostsRef);
+		// 	allPosts[userId] = {};
+		// 	postsSnapshot.forEach((postDoc) => {
+		// 		allPosts[userId][postDoc.id] = postDoc.data();
+		// 	});
+		// }
+		// console.log(allPosts);
+
+		let allPosts = {}; // for ONLY user2 (assume we only get back the current user data)
+		let currentUser = collection(db, `posts/user2/post`);
+		const snapShot = await getDocs(currentUser);
+		snapShot.forEach((doc) => {
+			allPosts[doc.id] = doc.data();
+		});
+		let res = [];
+		for (let key in allPosts) {
+			res.push(allPosts[key]);
+		}
+    res.sort((a, b) => {
+    return new Date(a.date) - new Date(b.date);
+  });
+		this.posts = res;
+	},
 };
 </script>
 <style scoped>
@@ -291,6 +381,13 @@ th,
 thead,
 tbody {
 	border: 2px solid black;
+}
+.logging-table th,
+.logging-table td {
+	min-width: 100px;
+	/* border: 1px solid #ccc; */
+	text-align: left;
+	padding: 10px;
 }
 @media (max-width: 768px) {
 	.desktop-tabs {
