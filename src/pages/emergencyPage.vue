8c41d8f33fa0f7d1e3ca3615805c005ddb7e4ce2
LeTroller <eugeneteowj@gmail.com>
2024-10-16 00:28:37 +0800

    Changes to form section, start on adding functions to find user location

--- src/pages/emergencyPage.vue
+++ src/pages/emergencyPage.vue
@@ -1,7 +1,24 @@
 <script setup>
 import NavBar from "../components/navBar.vue";
 import CustomHeader from "../components/CustomHeader.vue";
-//import GoogleMapsLoader from 'google-maps';
+import { ref } from 'vue';
+
+//Current function to get user's current location, to add more functions later once Vue3 geolocation library is updated
+const lat = ref(0);
+const long = ref(0);
+window.onload = getCurrentLocation;
+
+function getCurrentLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      lat.value = position.coords.latitude
+      long.value = position.coords.longitude
+      //initMap(latitude, longitude);
+    })
+  } else {
+    alert("Error!")
+  }
+}
 </script>
 
 <template>
@@ -18,19 +35,19 @@ import CustomHeader from "../components/CustomHeader.vue";
 
   <!-- Map block divider -->
   <div class="map-block p-3">
-    <CustomHeader header="FIND YOUR NEAREST A&E"></CustomHeader>
-    <h1>Insert map here</h1>
-    <!-- May not need form -->
+    <CustomHeader header="FIND YOUR NEAREST A&E" />
+    <div id="map">
+      <img src="../assets/AnE.jpg" class="w-75"> <!-- Placeholder for map, rough gauge of map dimensions -->
+    </div>
+    <!-- When users click the button, the web will find nearest A&E location -->
     <form id="searchLocation">
-      Enter your location (region): <input type="text" class="bg-light rounded-3" id="locationName">
-      <button type="button" class="btn btn-success m-2 p-1" onsubmit="">Submit</button>
-      <br>Nearest A&E: <span id="nearestLocations"></span> <!-- Change text content when region has nearby A&Es -->
+      <button type="button" class="btn btn-success m-2 p-1">Find nearest A&E</button>
     </form>
   </div>
 
   <!-- Contact list divider -->
   <div class="contacts p-3">
-    <CustomHeader header="CONTACTS"></CustomHeader>
+    <CustomHeader header="CONTACTS" />
     <!-- Contact numbers displayed inline in same row as respective services, each service type separated into newlines -->
     <div class="contacts-list">
       <span class="service-type">AMBULANCE SERVICES:</span><p class="contact d-inline"> 995</p><br>
@@ -40,21 +57,30 @@ import CustomHeader from "../components/CustomHeader.vue";
   </div>
 
 </template>
-  
+
 <script>
   export default {
-    name: 'emergencyPage'
+    name: 'emergencyPage',
+    components: {
+		  CustomHeader
+	  },
+    data() {
+      return {
+        apiKey: "AIzaSyATGD0drDahbJeZQOPtXkzm2Qid75e4Mww"
+      }
+    }
   };
   /*
   To Do: Figure out how to generate map using search.html and display as a mini map block
   - Get user current location and pin it on their maps
   - Pin nearest A&Es, as well as display it 
-  - If nearest A&Es are calculated based on user location (lat & long), may not need form
+  - If nearest A&Es are calculated based on user location (lat & long), may not need form (Settled -> Changed to only
+  include one button that will find nearest A&E by finding min diff in lat and long between user location and A&E location)
+  - Place pin on nearest A&E location
 
   Plans for locating nearest A&E:
   1) Gather all A&E locations and group all locations in object based on region -> locations = {LocA: [], LocB: [], ...}
-  2) If region exists, gather all locations in region and show to users in 'Nearest A&E' span
-  3) If region does not exist, show error message in 'Nearest A&E' span
+  2) Using latitude and longitude, calculate nearest A&E (min diff in lat and long)
   */
 </script> 
 
@@ -63,6 +89,10 @@ import CustomHeader from "../components/CustomHeader.vue";
     object-fit: cover;
     width: 100%;
     height: 400px;
+  }
+
+  button {
+    font-family: "Cherry Bomb", sans-serif;
   }
 
   .contacts-list {
