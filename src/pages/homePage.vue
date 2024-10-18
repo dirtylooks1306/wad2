4d566136381567e3f4c6326ae68a2b2d8a96ed94
Li Quan <liquan2808@gmail.com>
2024-10-18 11:13:31 +0800

    updated homepage.vue (removed export)

--- src/pages/homePage.vue
+++ src/pages/homePage.vue
@@ -2,7 +2,53 @@
 import CustomHeader from "../components/CustomHeader.vue";
 import Carousel from "../components/Carousel.vue";
 import NavBar from "../components/navBar.vue";
+import Forum from "../components/Forum.vue";
+
+// Router instance for navigation
+const carouselItems = [
+  {
+    title: "Title 1",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium mi at tincidunt accumsan.",
+    image: "src/assets/placeholder.svg",
+    alt: "Descriptive Alt Text",
+    icon1: "src/assets/heart.svg",
+    icon2: "src/assets/bookmark.svg",
+  },
+  {
+    title: "Title 2",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium mi at tincidunt accumsan.",
+    image: "/src/assets/placeholder.svg",
+    alt: "Descriptive Alt Text",
+    icon1: "src/assets/heart.svg",
+    icon2: "src/assets/bookmark.svg",
+  },
+  {
+    title: "Title 3",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium mi at tincidunt accumsan.",
+    image: "src/assets/placeholder.svg",
+    alt: "Descriptive Alt Text",
+    icon1: "src/assets/heart.svg",
+    icon2: "src/assets/bookmark.svg",
+  },
+];
+
+const forumPosts = [
+  {
+    id: 1,
+    icon: 'fa-car',
+    title: 'Description Title',
+    link: '#',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    postsCount: 24,
+    topicsCount: 12,
+    lastPostLink: '#',
+    lastPoster: 'JustAUser',
+    lastPosterLink: '#',
+    lastPostDate: '12 Dec 2020'
+  },
+];
 </script>
+
 <template>
 	<NavBar />
 	<header>
@@ -14,7 +60,9 @@ import NavBar from "../components/navBar.vue";
 						<span class="line-2">Nurturing Futures</span>
 					</div>
 					<br />
-					<button class="btn btn-primary">Get Started</button>
+					<router-link to="/diary">
+						<button class="btn btn-primary">Get Started</button>
+					</router-link>
 				</div>
 				<div class="col-md-6">
 					<img src="../assets/HomeMasthead.svg" alt="baby" />
@@ -34,7 +82,7 @@ import NavBar from "../components/navBar.vue";
 		<CustomHeader header="GrowthTracker" />
 		<div class="row">
 			<div class="col-md-1"></div>
-			<div class="col-md-4">
+			<div class="col-md-4 col-12 text-start">
 				<div class="title-class">
 					Begin Your Little One's Journey to Healthy Growth!
 				</div>
@@ -42,11 +90,11 @@ import NavBar from "../components/navBar.vue";
 					Stay informed and engaged—check out the Growth Tracker to
 					celebrate every milestone!
 				</p>
-				<a href="#"
-					><button class="btn btn-primary">GrowthTracker</button></a
-				>
+				<router-link to="/tracker">
+					<button class="btn btn-primary">GrowthTracker</button>
+				</router-link>
 			</div>
-			<div class="col-md-6">
+			<div class="col-md-6 col-12">
 				<img
 					src="../assets/homepageBaby.jpg"
 					alt="Growth Tracker"
@@ -59,54 +107,12 @@ import NavBar from "../components/navBar.vue";
 
 	<div class="container-fluid secondary-background p-3">
 		<CustomHeader header="Forum" />
+		<Forum v-for="post in forumPosts" :key="post.id" :post="post" />
 	</div>
 	<footer>
 		<p>CradleCare © 2024</p>
 	</footer>
 </template>
-
-<script>
-export default {
-	name: "homePage",
-	components: {
-		CustomHeader,
-		Carousel,
-	},
-	data() {
-		return {
-			carouselItems: [
-				{
-					title: "Title 1",
-					description:
-						"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium mi at tincidunt accumsan.",
-					image: "src/assets/placeholder.svg",
-					alt: "Descriptive Alt Text",
-					icon1: "src/assets/heart.svg",
-					icon2: "src/assets/bookmark.svg",
-				},
-				{
-					title: "Title 2",
-					description:
-						"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium mi at tincidunt accumsan.",
-					image: "/src/assets/placeholder.svg",
-					alt: "Descriptive Alt Text",
-					icon1: "src/assets/heart.svg",
-					icon2: "src/assets/bookmark.svg",
-				},
-				{
-					title: "Title 3",
-					description:
-						"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium mi at tincidunt accumsan.",
-					image: "src/assets/placeholder.svg",
-					alt: "Descriptive Alt Text",
-					icon1: "src/assets/heart.svg",
-					icon2: "src/assets/bookmark.svg",
-				},
-			],
-		};
-	},
-};
-</script>
 
 <style scoped>
 .carousel-image {
@@ -152,14 +158,6 @@ img {
 .intro,
 .features {
 	margin: 20px 0;
-}
-
-.feature-card {
-	display: inline-block;
-	border: 1px solid #ddd;
-	padding: 10px;
-	margin: 10px;
-	width: 30%;
 }
 
 footer {
