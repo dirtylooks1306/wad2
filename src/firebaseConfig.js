4e3edbf19c729c1dcc25e72fbcb6bb976b254c11
LeTroller <eugeneteowj@gmail.com>
2024-11-09 17:54:47 +0800

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword , sendPasswordResetEmail, signOut} from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js';
-import { getFirestore, collection, addDoc, doc, setDoc, getDoc, updateDoc, deleteDoc, getDocs, query, where,onSnapshot, arrayRemove, arrayUnion, orderBy, Timestamp, limit } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js';
+import { getFirestore, collection, addDoc, doc, setDoc, getDoc, updateDoc, deleteDoc, getDocs, query, where,onSnapshot, arrayRemove, arrayUnion, orderBy, Timestamp, limit, writeBatch } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js';
 import { getStorage, ref, uploadBytesResumable, getDownloadURL, uploadBytes, listAll, deleteObject } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-storage.js";
 
 // import { getAnalytics } from "firebase/analytics";
